6dc489a05bf7a4d81340f1ef75e5be5c51e2fc3e
Mihai Bazon <mihai@bazon.net>
2011-04-10 20:45:15 +0300

    implemented a reader which is more CL-like (looks like it makes no sense to talk about a tokenization step here; the reader needs to be very customizable)

--- jisp.js
+++ jisp.js
@@ -1,19 +1,89 @@
-function with_input_from_string($SOURCE) {
+var sys = require("sys");
+
+var _READTABLE_;
+var _PACKAGE_;
+var _ALL_PACKAGES_ = {};
+
+function Symbol(pack, name) {
+        this._package = pack;
+        this._name = name;
+        this._plist = {};
+};
+
+Symbol.prototype = {
+        toString: function() {
+                return this._package + ":" + this._name;
+        }
+};
+
+function Package(name) {
+        _ALL_PACKAGES_[name.toUpperCase()] = this;
+        this._name = name;
+        this._symbols = {};
+};
+
+Package.prototype = {
+        symbol: function(name) {
+                return this._symbols[name] ||
+                        (this._symbols[name] = new Symbol(this, name));
+        },
+        discard_symbol: function(name) {
+                if (name instanceof Symbol) name = name._name;
+                delete this._symbols[name];
+        }
+};
+
+new Package("CL");
+_PACKAGE_ = new Package("CL-USER");
+
+function get_package(name) {
+        return _ALL_PACKAGES_[name.toUpperCase()];
+};
+
+function make_string_stream($SOURCE) {
         var $line = 0, $col = 0, $pos = 0;
-        function peek() { return $SOURCE.charAt($pos) };
+        function rest() {
+                return $SOURCE.substr($pos);
+        };
+        function peek(type) {
+                return $SOURCE.charAt($pos);
+        };
         function next() {
                 var ch = $SOURCE.charAt($pos++);
                 if (ch == "\n") $col = 0, ++$line;
                 else ++$col;
                 return ch;
         };
+        function skip_ws() {
+                read_while(function(ch){
+                        switch (ch) {
+                            case " ":
+                            case "\t":
+                            case "\n":
+                            case "\x0C":
+                            case "\u2028":
+                            case "\u2029":
+                                return true;
+                        }
+                });
+        };
+        function read_while(test) {
+                var str = "";
+                while (test(peek()))
+                        str += next();
+                return str;
+        };
+        var backquote = 0;
+        function with_backquote(cont) {
+                ++backquote;
+                try { return cont() }
+                finally { --backquote }
+        };
         return {
                 get line() { return $line },
                 get col() { return $col },
                 get pos() { return $pos },
-                get peek() { return peek() },
-                get next() { return next() },
-                get rest() { return $SOURCE.substr($pos) },
+                get in_backquote() { return backquote > 0 },
 
                 error: function(text) {
                         throw new Error(text + "\nLine: " + $line
@@ -21,153 +91,164 @@ function with_input_from_string($SOURCE) {
                                         + ", Pos: " + $pos);
                 },
 
-                read_while: function(test) {
-                        var str = "";
-                        while (test(peek()))
-                                str += next();
-                        return str;
+                next: next,
+                peek: peek,
+                skip_ws: skip_ws,
+                read_while: read_while,
+                with_backquote: with_backquote,
+        };
+};
+
+function HOP(obj, prop) {
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+};
+
+var WHITESPACE_CHARS = {
+        " ": true,
+        "\t": true,
+        "\n": true,
+        "\x0C" : true,
+        "\u2028": true,
+        "\u2029": true
+};
+
+function is_whitespace(ch) {
+        return HOP(WHITESPACE_CHARS, ch);
+};
+
+var SPECIAL_CHARS = {
+        "(": read_standard_list,
+        '"': read_string,
+        "'": read_quote,
+        "`": read_backquote,
+        ",": read_comma,
+        "|": read_pipe,
+        "#": read_sharp,
+
+        ";": ignore_comment,
+
+        ")": function(stream) {
+                stream.error("unmatched close parenthesis");
+        },
+
+        // whitespace is ignored
+        " ": ignore,
+        "\t": ignore,
+        "\n": ignore,
+        "\x0C" : ignore,
+        "\u2028": ignore,
+        "\u2029": ignore
+};
+
+function ignore(stream) {
+        stream.next();
+};
+
+function ignore_comment(stream) {
+        read_while(function(ch){ return ch == "\n" });
+};
+
+function read_delimited_list(stream, endchar) {
+        var list = [];
+        while (true) {
+                stream.skip_ws();
+                if (stream.peek() == endchar) {
+                        stream.next();
+                        return list;
+                } else {
+                        var el = read(stream);
+                        if (el != null) list.push(el);
                 }
-        };
+        }
 };
 
-function make_reader() {
-
-        function HOP(obj, prop) {
-                return Object.prototype.hasOwnProperty.call(obj, prop);
-        };
-
-        var WHITESPACE_CHARS = {
-                " ": true,
-                "\t": true,
-                "\n": true,
-                "\x0C" : true,
-                "\u2028": true,
-                "\u2029": true
-        };
-
-        function is_whitespace(ch) {
-                return HOP(WHITESPACE_CHARS, ch);
-        };
-
-        var SPECIAL_CHARS = {
-                "(": true,
-                ")": true,
-                '"': true,
-                "'": true,
-                "`": true,
-                ",": true,
-                "|": true,
-                "\\": true,
-                ":": true,
-                "#": true
-        };
-
-        function is_special(ch) {
-                return HOP(SPECIAL_CHARS, ch);
-        };
-
-        function tokenizer($SOURCE) {
-                var stream = with_input_from_string($SOURCE);
-                var token;
-
-                function start_token() {
-                        return token = { line: stream.line,
-                                         col: stream.col,
-                                         pos: stream.pos };
-                };
-
-                function as(type, val) {
-                        token.type = type;
-                        token.val = val;
-                        return token;
-                };
-
-                function error(text) {
-                        stream.error(text);
-                };
-
-                function next() { return stream.next };
-
-                function skip(ch) {
-                        if (stream.peek != ch) error("Expected '" + ch + "'");
-                        next();
-                };
-
-                function skip_ws() {
-                        stream.read_while(is_whitespace);
-                };
-
-                function read_string() {
-                        skip('"');
-                        var str = "", esc = false;
-                        while (true) {
-                                var ch = next();
-                                if (esc) {
-                                        str += ch;
-                                        esc = false;
-                                } else if (ch == "\\") {
-                                        esc = true;
-                                } else if (ch == '"') {
-                                        return str;
-                                } else if (ch == null) {
-                                        error("Unterminated string");
-                                } else {
-                                        str += ch;
-                                }
-                        }
-                };
-
-                function read_comment() {
-                        skip(";");
-                        return stream.read_while(function(ch){
-                                return ch != "\n";
-                        });
-                };
-
-                function read_token() {
-                        var esc = false, tok = "", ch;
-                        while (ch = stream.peek) {
-                                if (esc) tok += next(), esc = false;
-                                else if (ch == "\\") next(), esc = true;
-                                else if (is_whitespace(ch) || is_special(ch)) break;
-                                else tok += next();
-                        }
-                        return tok;
-                };
-
-                var parens = [];
-                return function() {
-                        skip_ws();
-                        start_token();
-                        var ch = stream.peek;
-                        if (!ch) return as("eof");
-                        switch (ch) {
-                            case '"': return as("string", read_string());
-                            case ";": return as("comment", read_comment());
-                            case "'": return as("quote", next());
-                            case "`": return as("backquote", next());
-                            case ",": return as("comma", next());
-                            case "(": return as("lparen", next());
-                            case ")": return as("rparen", next());
-                            case ":": return as("colon", next());
-                        }
-                        var token = read_token();
-                        if (/^[0-9]*\.?[0-9]+$/.test(token))
-                                return as("number", parseFloat(token));
-                        return as("token", token);
-                };
-        };
-
-        return tokenizer;
-
+function read_standard_list(stream) {
+        return [ "LIST", read_delimited_list(stream, ")") ];
 };
 
+function read_escaped(stream, endchar) {
+        var esc = false, str = "";
+        while (true) {
+                var ch = stream.next();
+                if (!ch) stream.error("Unterminated literal");
+                if (esc) { str += ch; esc = false; }
+                else if (ch == "\\") esc = true;
+                else if (ch == endchar) return str;
+                else str += ch;
+        }
+};
+
+function read_string(stream) {
+        return [ "STRING", read_escaped(stream, '"') ];
+};
+
+function read_quote(stream) {
+        return [ "QUOTE", read(stream) ];
+};
+
+function read_backquote(stream) {
+        return stream.with_backquote(function(){
+                return [ "BACKQUOTE", read(stream) ];
+        });
+};
+
+function read_comma(stream) {
+        if (!stream.in_backquote)
+                stream.error("Comma not inside backquote");
+        return [ "UNQUOTE", read(stream) ];
+};
+
+function read_pipe(stream) {
+        return [ "SYMBOL", read_escaped(stream, "|") ];
+};
+
+function read_sharp(stream) {
+        // TBD
+};
+
+function read_symbol(stream) {
+        var esc = false, colon = null, str = "";
+        while (true) {
+                var ch = stream.peek();
+                if (esc) { str += stream.next(); esc = false; }
+                else if (ch == ":") {
+                        if (colon != null) stream.error("too many colons");
+                        colon = str.length; str += stream.next();
+                }
+                else if (ch == "\\") { esc = true; stream.next(); }
+                else if (HOP(SPECIAL_CHARS, ch)) break;
+                str += stream.next();
+        }
+        if (/^[0-9]*\.?[0-9]+$/.test(str)) {
+                return [ "NUMBER", parseFloat(str) ];
+        }
+        str = str.toUpperCase();
+        var pack = _PACKAGE_, n = str;
+        if (colon) {
+                var p = str.substr(0, colon);
+                n = str.substr(colon + 1);
+                pack = get_package(p);
+                if (!pack) stream.error("no package " + p);
+        }
+        return [ "SYMBOL", pack.symbol(n) ];
+};
+
+function read(stream, eof_error, eof_value) {
+        if (arguments.length == 1) eof_error = true;
+        var ch = stream.peek();
+        if (!ch) {
+                if (eof_error) stream.error("end of input");
+                return eof_value;
+        }
+        var reader = SPECIAL_CHARS[ch];
+        if (reader) {
+                stream.next();
+                return reader(stream);
+        }
+        return read_symbol(stream);
+};
 
 var str = "(defun f\\(oo(a b c) (cl:format nil \"foo ~a ~a ~a\" \n\
-a b (+ c 0.5 0.3.4)))";
-var token = make_reader()(str);
-while (true) {
-        var t = token();
-        if (t.type == "eof") break;
-        console.log(t);
-}
+a b (+ c '0.5 0.3.4)))";
+console.log( sys.inspect( read(make_string_stream(str)), null, null ) );
+//console.log( read(make_string_stream(str)) );
