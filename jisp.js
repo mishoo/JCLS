402b06d8ac42b401a083b406a89016d505e8435c
Mihai Bazon <mihai@bazon.net>
2011-05-07 16:10:43 +0300

    removed some unused utilities

--- jisp.js
+++ jisp.js
@@ -6,40 +6,6 @@ function compose(a, rest) {
         return function() { return a(rest.apply(this, arguments)) };
 };
 
-function range(start, stop) {
-        var a = [];
-        for (var i = start, j = 0; i <= stop; ++i, ++j)
-                a[j] = i;
-        return a;
-};
-
-var amb = (function(){
-        function fail() { throw fail }
-        function amb(values, program, fail_value) {
-                if (!values) fail();
-                var n = values.length;
-                if (n == 0) fail();
-                for (var i = 0; i < n; ++i) try {
-                        return program(values[i]);
-                } catch(ex) {
-                        if (ex !== fail) throw ex;
-                }
-                if (fail_value === fail) fail();
-                return fail_value;
-        }
-        amb.fail = fail;
-        return amb;
-})();
-
-function repeat_string(str, i) {
-        if (i <= 0) return "";
-        if (i == 1) return str;
-        var d = repeat_string(str, i >> 1);
-        d += d;
-        if (i & 1) d += str;
-        return d;
-};
-
 function defaults(args, defs) {
         var ret = {};
         if (args === true)
