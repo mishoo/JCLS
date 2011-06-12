1958d5774a85d743d77f0340b4f442c4d62df76d
Mihai Bazon <mihai@bazon.net>
2011-06-13 01:29:52 +0300

    minor

--- test-node.js
+++ test-node.js
@@ -1,15 +1,16 @@
 var fs = require("fs");
 var path = require("path");
 var JCLS = require("./jcls");
+var sys = require("sys");
 
 function eval_file(filename) {
-        console.log("* Loading", filename);
+        sys.debug("* Loading " + filename);
         var time = new Date().getTime();
         JCLS.eval_string(
                 fs.readFileSync(filename).toString(),
                 function succeed(ret) {
                         var diff = (new Date().getTime() - time) / 1000
-                        console.log("  ==>", JCLS.write_ast_to_string(ret), " in ", diff.toFixed(3), "s.");
+                        sys.debug("  ==> " + JCLS.write_ast_to_string(ret) + " in " + diff.toFixed(3) + "s.\n");
                 }
         );
 };
